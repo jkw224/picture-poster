6701ad542a56e5feb986d447cdfdea334af9e61c
Jonathan Wood <jonathankimballwood@gmail.com>
2015-10-19 18:34:25 -0600

    bug in code -- requesting Mark's help -- empty function is not convertible to Response AnyObject, NSError

--- picture-poster/FeedCell.swift
+++ picture-poster/FeedCell.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class FeedCell: UITableViewCell {
 
     @IBOutlet weak var profileImg: UIImageView!
-    @IBOutlet weak var showCaseImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
     @IBOutlet weak var descriptionText: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
     
     var post: Post!
+    var request: Request!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,7 +28,7 @@ class FeedCell: UITableViewCell {
         profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
         profileImg.clipsToBounds = true
         
-        showCaseImg.clipsToBounds = true
+        showcaseImg.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -35,11 +37,31 @@ class FeedCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage?) {
         self.post = post
         
         self.descriptionText.text = post.postDescription
         self.likesLbl.text = "\(post.likes)"
+        
+        if post.imageUrl != nil {
+            
+            if img != nil {
+                self.showcaseImg.image = img
+            } else {
+                request = Alamofire.request(.GET, post.imageUrl!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, err in
+                    
+                    if err == nil {
+                        let img = UIImage(data: data!)!
+                        self.showcaseImg.image = img
+                        FeedVC.imageCache.setObject(img, forKey: self.post.imageUrl!)
+                    }
+                    
+                })
+            }
+            
+        } else {
+            self.showcaseImg.hidden = true
+        }
         
     }
 }
--- picture-poster/FeedVC.swift
+++ picture-poster/FeedVC.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    var posts = [Post]()
+    @IBOutlet weak var postField: MaterialTextField!
+    @IBOutlet weak var uploadImage: UIImageView!
     
+    var posts = [Post]()
+    static var imageCache = NSCache()
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 361
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
@@ -58,7 +67,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let post = posts[indexPath.row]
         
         if let cell = tableView.dequeueReusableCellWithIdentifier("FeedCell") as? FeedCell {
-            cell.configureCell(post)
+            
+            cell.request?.cancel()
+            
+            var img: UIImage?
+            
+            if let url = post.imageUrl {
+                img = FeedVC.imageCache.objectForKey(url) as? UIImage
+            }
+            
+            cell.configureCell(post, img: img)
             return cell
         } else {
             return FeedCell()
@@ -68,14 +86,70 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // get rid of -> above will never be called
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let post = posts[indexPath.row]
+        
+        if post.imageUrl == nil {
+            return 165
+        } else {
+            return tableView.estimatedRowHeight
+        }
     }
-    */
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        uploadImage.image = image
+    }
 
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func makePost(sender: AnyObject) {
+        
+        if let txt = postField.text where txt != "" {
+         
+            if let img = uploadImage.image {
+                let urlStr = "https://post.imageshack.us/upload_api.php"
+                let url = NSURL(string: urlStr)!
+                let imgData = UIImageJPEGRepresentation(img, 0.2)! //compress image into JPEG!! Alamo fire requires JPEG
+                let keyData = "3789GLOT4c2fa0ae906f3fae3ad5696ddb0c5233".dataUsingEncoding(NSUTF8StringEncoding)!
+                let keyJSON = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                
+                Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                    
+                    multipartFormData.appendBodyPart(data: imgData, name: "fileUpload", fileName: "image", mimeType: "image/jpg")
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJSON, name: "format")
+                    
+                    }) { encodingResult in
+                    
+                        switch encodingResult {
+                        case .Success(let upload, _, _):
+                            upload.responseJSON(completionHandler: { request, response, result in
+                                
+                                if let info = result.value as? Dictionary<String, AnyObject> {
+                                    
+                                    if let links = info["links"] as? Dictionary<String, AnyObject> {
+                                        if let imgLink = links["image_link"] as? String {
+                                            
+                                        }
+                                    }
+                                    
+                                }
+                                
+                            })
+                        case .Failure(let error):
+                            print(error)
+                        
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
 }
